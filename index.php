9de9350ca2339e7ce7c7032183625fb55c1fd03d
parveshsagar <86065634+parveshsagar@users.noreply.github.com>
2022-01-17 14:44:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,356 @@
 
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://creativeorion.com/wp-content/themes/creative-orion/assets/css/animate.css">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="responsive.css">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <title>Portfolio</title>
+</head>
+
+<body>
+<!--   <?php 
+    if($_GET['status'] == 'sent'){
+        echo "<script>swal({title: 'Thank You', text: 'Mail sent Successfully', type: 'success'}).then(function(){window.location = 'https://parveshsagar.github.io/parvesh-portfolio/' })</script>";
+    }
+    elseif($_GET['status'] == 'failed'){
+        echo "<script>swal({title: 'Sorry', text: 'Mail could not be sent. Please try again', type: 'failed'}).then(function(){window.location = 'https://parveshsagar.github.io/parvesh-portfolio/' })</script>";
+    }else{
+        
+    }
+
+?> -->
+  <canvas id='world'></canvas>
+  <section class="header-area">
+    <div class="side-navbar">
+      <div class="image">
+        <img src="images/p-logo.png">
+      </div>
+      <div class="name">
+        <h3>Parvesh</h3>
+      </div>
+      <div class="designation ">
+        <p>Web Developer</p>
+      </div>
+
+      <div class="sidebar-links">
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">My Skills</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#"><i class="fab fa-linkedin-in"></i></a><a href="#" class="github"><i
+                class="fab fa-github"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <div class="toggle-btn"><i class="fas fa-bars"></i></div>
+  <section class="intro">
+
+    <div class="text">
+      <h1 aria-label="Hi,I'm Parvesh, 
+          web developer">
+        <span class="blast" aria-hidden="true" style="opacity: 1;">H</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">i</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">,</span>
+        <br>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">I</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">'</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">m</span>
+        <span class="blast" aria-hidden="true" style="opacity: 0;">'</span>
+        <img src="images/p-logo.png" class="p">
+        <span class="blast " aria-hidden="true" style="opacity: 0;">P</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">a</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">r</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">v</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+        <span class="blast " aria-hidden="true" style="opacity: 1;">s</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">h</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">,</span>
+        <br>
+        <span class="blast " aria-hidden="true" style="opacity: 1;">w</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">b</span>
+        <span class="blast" aria-hidden="true" style="opacity: 0;">'</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">d</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">v</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">l</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">p</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">r</span>
+      </h1>
+      <p>Front End Developer / Wordpress developer</p>
+
+      <a href="#">Contact me !</a>
+    </div>
+    <div class="scroll-down">
+      <span>scroll down</span>
+      <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-arrow-down fa-w-14 fa-5x"><path fill="currentColor" d="M443.5 248.5l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L241 419.9V44c0-6.6-5.4-12-12-12h-10c-6.6 0-12 5.4-12 12v375.9L28.5 241.4c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.8 4.8-12.3.1-17z" class=""></path></svg>
+  </div>
+    <div class="scroll-down scroll-down--left">
+      <span>scroll down</span>
+      <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-arrow-down fa-w-14 fa-5x"><path fill="currentColor" d="M443.5 248.5l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L241 419.9V44c0-6.6-5.4-12-12-12h-10c-6.6 0-12 5.4-12 12v375.9L28.5 241.4c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.8 4.8-12.3.1-17z" class=""></path></svg>
+  </div>
+  </section>
+  
+  <section class="work">
+    <div class="headings">
+      <h1 class="heading">
+        <span class="blast" aria-hidden="true" arstyle="opacity: 1;">M</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">y</span>
+        <span class="blast" aria-hidden="true" style="opacity: 0;">'</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">P</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">r</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">t</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">f</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">l</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">i</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus a quo dolorum eaque ipsa quia minus est
+        possimus similique modi non voluptatum nisi labore harum necessitatibus eveniet, distinctio nam enim?</p>
+    </div>
+
+    <div class="fake-big">Work</div>
+    
+  </section>
+  <section class="port-gallery">
+    <div class="portfolio">
+      <div class="container-fluid folio">
+        <div class="row">
+          <div class="col-lg-3 col-md-4 port">
+            <div class="content">
+              <div class="images">
+                <a data-fancybox="gallery" class="gallery" data-src="images/mysap.png" data-width="640"
+                  data-height="360" data-caption>
+                  <img src="images/mysap.png" width="200" height="150" />
+                  <div class="overlay-photo-work-1"></div>
+                  <div class="overlay-photo-work-2"></div>
+                  <div class="zoom">
+                    <i class="fa fa-search-plus"></i>
+                  </div>
+                </a>
+
+              </div>
+              <a class="visit" href="http://www.mysaptutorials.com/">Visit Here</a>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-4 port">
+            <div class="content">
+              <div class="images">
+                <a data-fancybox="gallery" class="gallery" data-src="images/roi10x.png" data-width="640"
+                  data-height="360" data-caption>
+                  <img src="images/roi10x.png" width="200" height="150" />
+                  <div class="overlay-photo-work-1"></div>
+                  <div class="overlay-photo-work-2"></div>
+                  <div class="zoom">
+                    <i class="fa fa-search-plus"></i>
+                  </div>
+                </a>
+
+              </div>
+              <a class="visit" href="http://www.roi10x.com/">Visit Here</a>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-4 port">
+            <div class="content">
+              <div class="images">
+                <a data-fancybox="gallery" class="gallery" data-src="images/mysap.png" data-width="640"
+                  data-height="360" data-caption>
+                  <img src="images/corevalues.png" width="200" height="150" />
+                  <div class="overlay-photo-work-1"></div>
+                  <div class="overlay-photo-work-2"></div>
+                  <div class="zoom">
+                    <i class="fa fa-search-plus"></i>
+                  </div>
+                </a>
+
+              </div>
+              <a class="visit" href="#">Visit Here</a>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-4 port">
+            <div class="content">
+              <div class="images">
+                <a data-fancybox="gallery" class="gallery" data-src="images/mysap.png" data-width="640"
+                  data-height="360" data-caption>
+                  <img src="images/contact.PNG" width="200" height="150" />
+                  <div class="overlay-photo-work-1"></div>
+                  <div class="overlay-photo-work-2"></div>
+                  <div class="zoom">
+                    <i class="fa fa-search-plus"></i>
+                  </div>
+                </a>
+
+              </div>
+              <a class="visit" href="#">Visit Here</a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 port">
+            <div class="content">
+              <div class="images">
+                <a data-fancybox="gallery" class="gallery" data-src="images/mysap.png" data-width="640"
+                  data-height="360" data-caption>
+                  <img src="images/register.PNG" width="200" height="150" />
+                  <div class="overlay-photo-work-1"></div>
+                  <div class="overlay-photo-work-2"></div>
+                  <div class="zoom">
+                    <i class="fa fa-search-plus"></i>
+                  </div>
+                </a>
+
+              </div>
+              <a class="visit" href="#">Visit Here</a>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-4 port">
+            <div class="content">
+              <div class="images">
+                <a data-fancybox="gallery" class="gallery" data-src="images/mysap.png" data-width="640"
+                  data-height="360" data-caption>
+                  <img src="images/blogs.png" width="200" height="150" />
+                  <div class="overlay-photo-work-1"></div>
+                  <div class="overlay-photo-work-2"></div>
+                  <div class="zoom">
+                    <i class="fa fa-search-plus"></i>
+                  </div>
+                </a>
+
+              </div>
+              <a class="visit" href="#">Visit Here</a>
+            </div>
+
+          </div>
+
+        </div>
+        
+      </div>
+    </div>
+    
+  </section>
+  <section class="contact-me">
+    <div class="headings">
+      <h1 class="heading">
+        <span class="blast" aria-hidden="true" arstyle="opacity: 1;">C</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">n</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">t</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">a</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">c</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">t</span>
+        <span class="blast" aria-hidden="true" style="opacity: 0;">'</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">m</span>
+        <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+
+
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus a quo dolorum eaque ipsa quia minus est
+        possimus similique modi non voluptatum nisi labore harum necessitatibus eveniet, distinctio nam enim?</p>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8  ">
+          <form class="form" id="contactparvesh" method="post" action="action.php">
+            <div class="row">
+              <div class="col-lg-6 col-md-6">
+                <div class="form-group ">
+                  <input type="text" class="form-control" placeholder="Name" id="name" name="name">
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Phone" id="phone" name="phone">
+                </div>
+              </div>
+            </div>
+              <div class="form-group">
+                <input type="email" class="form-control" placeholder="Email" id="email" name="email">
+              </div>
+              <div class="form-group">
+                <textarea type="text" rows="6" cols="50" class="form-control" placeholder="Message" id="message" name="message"></textarea>
+              </div>
+              <div class="form-button">
+                <input type="submit" value="SUBMIT" name="submit" class="btn-primary f14">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+  
+  <!--
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+<script src="portfolio.js"></script>
+<!-- Optional JavaScript; choose one of the two! -->
+<script>
+  $(document).ready(function () {
+            $('.toggle-btn').click(function () {
+                $(this).siblings('.header-area').toggleClass('active');
+
+                
+
+                if ($(this).hasClass('toggle-open')) {
+                    $(this).removeClass('toggle-open').addClass('toggle-close');
+                    $(".toggle-btn i").removeClass("fa-times").addClass("fa-bars")
+                } else {
+                    $(this).removeClass('toggle-close').addClass('toggle-open');
+                    $(".toggle-btn i").removeClass("fa-bars").addClass("fa-times")
+                }
+            });
+        });
+</script>
+<script>
+  Fancybox.bind(".gallery", {
+    // Your options go here'slideShow',
+    buttons: [
+      "share",
+      "fullScreen",
+      "close",
+    ],
+    thumbs: {
+      autoStart: true
+    }
+  });
+
+</script>
+
+</html>
